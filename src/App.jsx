eb4ec3214d80c14aeeb471bf0db6d18f99d9c79d
yoga2000 <yogaraj3102000@gmail.com>
2024-04-06 07:30:33 +0530

    deleted json

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from "react";
 import Home from "./Pages/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import BlogDetails from "./Pages/BlogDetails";
 import Create from "./Pages/Create";
 import Update from "./Pages/Update";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/details/:id" element={<BlogDetails />} />
         <Route path="/create" element={<Create />} />
         <Route path="/update/:id" element={<Update />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 };
 
